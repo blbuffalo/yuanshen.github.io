104e3be65631afda4524c739a1d66f4422dd3b78
blbuffalo <98512044+blbuffalo@users.noreply.github.com>
2022-01-27 15:44:16 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -127,7 +127,7 @@
         _gameScore = 0;
         _gameOver = false;
         _gameStart = false;
-        _gameTimeNum = 60;
+        _gameTimeNum = 45;
         GameTimeLayer.innerHTML = creatTimeText(_gameTimeNum);
         countBlockSize();
         refreshGameLayer(GameLayer[0]);
@@ -344,8 +344,7 @@
     w.shareText = function(score) {
         let date2 = new Date();
         deviation_time = (date2.getTime() - _date1.getTime())
-        if (deviation_time > 23000) {
-            return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
+    
         }
         SubmitResults();
         if (score <= 49) return '逊呐';
